718634b1cc07f2e5824805b5e589145efca61ad7
MartaLourido <martalouri@gmail.com>
2025-03-13 22:32:02 +0100

    feat: persist event title and date between reloads

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EventInputComponent } from './features/event-input/event-input.component';
 import { DatePickerComponent } from './features/date-picker/date-picker.component';
 import { CountdownTimerComponent } from './features/countdown-timer/countdown-timer.component';
@@ -10,15 +10,22 @@ import { CountdownTimerComponent } from './features/countdown-timer/countdown-ti
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   eventTitle: string = '';
   formattedDate: string = '';
 
+  ngOnInit(): void {
+    this.eventTitle = localStorage.getItem('eventTitle') || '';
+    this.formattedDate = localStorage.getItem('eventDate') || '';
+  }
+
   updateEventTitle(newTitle: string) {
     this.eventTitle = newTitle;
+    localStorage.setItem('eventTitle', newTitle);
   }
 
   updateEventDate(newDate: string) {
     this.formattedDate = newDate;
+    localStorage.setItem('eventDate', newDate);
   }
 }
